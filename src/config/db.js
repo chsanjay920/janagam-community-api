24e4820735b45853daf0b3cbb765e67585e8960b
[your <61927341+chsanjay920@users.noreply.github.com>
2026-01-25 23:18:16 +0530

    mongo fails trail

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  });
+
   console.log("MongoDB connected");
 };
+
