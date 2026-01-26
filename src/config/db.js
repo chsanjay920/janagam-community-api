64c09337bb17fc1581ddbdbe67241941abefb95d
[your <61927341+chsanjay920@users.noreply.github.com>
2026-01-26 11:42:10 +0530

    db connection changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   await mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000,
+    ssl: true,
   });
-
   console.log("MongoDB connected");
 };
 
