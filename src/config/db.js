f2efa410bbe2d0e938820c6ebfd4527bc0b68383
[your <61927341+chsanjay920@users.noreply.github.com>
2026-01-26 12:05:35 +0530

    db connection corrected

--- src/config/db.js
+++ src/config/db.js
@@ -5,12 +5,14 @@ let isConnected = false;
 const connectDB = async () => {
   if (isConnected) return;
 
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI missing");
-  }
+  const uri = process.env.MONGO_URI;
 
-  const db = await mongoose.connect(process.env.MONGO_URI, {
+  const db = await mongoose.connect(uri, {
     bufferCommands: false,
+    serverSelectionTimeoutMS: 5000, 
+    socketTimeoutMS: 45000,
+    family: 4, 
+    tls: true,
   });
 
   isConnected = db.connections[0].readyState === 1;
