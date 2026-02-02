a0b54274d207d7e19a07e95bd1808d50f1510c6b
[your <61927341+chsanjay920@users.noreply.github.com>
2026-02-02 18:53:51 +0530

    Config Correction

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
-// Express app configuration
 const express = require("express");
+const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
-const cors = require('cors');
 
 const app = express();
-app.use(express.json());
 
-app.use(cors({
+const corsOptions = {
   origin: "https://janagam-community-web-test.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
-})); 
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); 
+
+app.use(express.json());
+
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use("/api", require("./routes/registration.routes"));
 
