d59e0bdb7c3b5fa6a520bd6a4ada5e0c1e27df35
[your <61927341+chsanjay920@users.noreply.github.com>
2026-02-08 16:23:27 +0530

    cors origin changes

--- src/app.js
+++ src/app.js
@@ -7,10 +7,12 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 
-app.use( cors({
-    origin: "https://janagam-community-web-test.vercel.app", 
+app.use(
+  cors({
+    origin: true,
     credentials: true,
-  })); 
+  })
+);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use("/api", require("./routes/registration.routes"));
 
