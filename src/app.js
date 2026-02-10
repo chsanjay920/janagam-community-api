3a0127780be1d885b12fca70b801ca6481d5b160
[your <61927341+chsanjay920@users.noreply.github.com>
2026-02-10 21:26:42 +0530

    Origin Site Url Added

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: process.env.SITE_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
